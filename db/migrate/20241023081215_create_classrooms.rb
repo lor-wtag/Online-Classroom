66165a90612e8a5d9fd0ecd7bfdf0b053ef8cd41
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-23 14:42:16 +0600

    Added model and migration for Classroom

---
+++ db/migrate/20241023081215_create_classrooms.rb
@@ -0,0 +1,15 @@
+class CreateClassrooms < ActiveRecord::Migration[7.2]
+  def up
+    create_table :classrooms do |t|
+      t.integer "user_id"
+      t.string "name"
+      t.string "course_code"
+      t.stirng "classroom_code"
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :classrooms
+  end
+end
