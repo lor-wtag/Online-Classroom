c67cd808238bd29ef8890e0756a276881fcd11cf
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-23 14:42:46 +0600

    Added model and migration for assignment

---
+++ db/migrate/20241023082904_create_assignments.rb
@@ -0,0 +1,16 @@
+class CreateAssignments < ActiveRecord::Migration[7.2]
+  def up
+    create_table :assignments do |t|
+      t.string "title"
+      t.text "description"
+      t.date "due_date"
+      t.integer "classroom_id"
+      t.integer "user_id"
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :assignments
+  end
+end
