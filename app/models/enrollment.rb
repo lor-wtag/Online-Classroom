1208767d8b8224d6a8d1a496f333a8aaa0aba5be
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-24 11:28:32 +0600

    Added required associations and validations to all the models

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,10 @@
 class Enrollment < ApplicationRecord
+  validates :student_id, presence: true
+  validates :classroom_id, presence: true
+
+  belongs_to :student, class_name: "User", foreign_key: "user_id"
+  belongs_to :classroom
+
+  # Ensure that a student can only enroll in a classroom once
+  validates_uniqueness_of :student_id, scope: :classroom_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   has_one_attached :profile_picture
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i
   enum role: { admin: 0, teacher: 1, student: 2 }
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, length: {in: 10..50}, format: {with: EMAIL_REGEX}, uniqueness: true
+  validates :role, presence: true, inclusion: { in: roles.keys }
+
+  has_many :classrooms, foreign_key: :teacher_id, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :enrollments, dependent: :destroy
+  has_many :classrooms, through: :enrollments
+
+
+
+
+
 end
