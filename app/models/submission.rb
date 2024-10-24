5232cd907b06e8fdcb1ce3bc7fb9bba3f20eb9d0
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-24 11:28:32 +0600

    Added required associations and validations to all the models

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,2 +1,10 @@
 class Submission < ApplicationRecord
+  belongs_to :assignment
+  belong_to :student, class_name: "User"
+
+  validates :assignment_id, presence: true
+  validates :student_id, presence: true
+  validates :file, presence: true
+  validates :grade, numericality: {allow_nil: true}
+  validates :feedback, length: {maximum: 300}, allow_nil: true
 end
