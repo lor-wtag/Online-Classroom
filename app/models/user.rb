469d3f2b5844049da4fb95590a76fcf53f0ee41c
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-27 10:50:07 +0600

    added and removed spaces for github lint acceptance

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i
   enum :role, { admin: 0, teacher: 1, student: 2 }
   validates :name, presence: true
-  validates :email, presence: true, length: {in: 10..50}, format: {with: EMAIL_REGEX}, uniqueness: true
+  validates :email, presence: true, length: { in: 10..50 }, format: { with: EMAIL_REGEX }, uniqueness: true
   validates :role, presence: true, inclusion: { in: roles.keys }
 
   has_many :classrooms, foreign_key: :user_id, dependent: :destroy
