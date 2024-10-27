7a8826baf9df40638e78c702c2667f255eb1f401
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-27 16:51:36 +0600

    minor changes for fixing rubocop errors

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module OnlineClassroom
     config.i18n.default_locale = :en
 
     # Optionally, load translations from subdirectories
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
