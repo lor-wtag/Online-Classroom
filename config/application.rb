6e6ca4715554deed6276e85a509f85e6dadf88d8
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-27 16:39:36 +0600

    made modifications for i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module OnlineClassroom
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :en
+
+    # Optionally, load translations from subdirectories
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
