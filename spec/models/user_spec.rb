004309e8abee2fd622a99262c76b37918e48dce7
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-27 10:22:25 +0600

    added rspec code for models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of (:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value("lamiya@gmail.com").for(:email) }
+    it { should_not allow_value("lamiya@").for(:email) }
+    it { should_not allow_value("lamiygmail.@com").for(:email) }
+    it { should validate_presence_of(:role) }
+    it { should define_enum_for(:role).with_values(%i[admin teacher student])}
+  end
+
+  describe "associations" do
+    it { should have_many(:classrooms).with_foreign_key(:user_id).dependent(:destroy) }
+    it { should have_many(:enrollments).dependent(:destroy) }
+    it { should have_many(:classrooms_as_student).through(:enrollments).source(:classroom) } # for students
+    it { should have_many(:posts).dependent(:destroy) }
+    it { should have_many(:comments).dependent(:destroy) }
+  end
+
+
+
+
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,9 +65,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  # Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
   end
 end
