1770b87dc30effd2418711968853beb9dbc57277
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-27 10:44:34 +0600

    Added some spaces and new lines

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:content)}
-    it { should validate_length_of(:content).is_at_most(500)}
+    it { should validate_presence_of(:content) }
+    it { should validate_length_of(:content).is_at_most(500) }
   end
 
   describe "associations" do
-    it { should belong_to(:commentable)}
-    it { should belong_to(:user)}
+    it { should belong_to(:commentable) }
+    it { should belong_to(:user) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,14 @@ RSpec.describe User, type: :model do
     it { should_not allow_value("lamiya@").for(:email) }
     it { should_not allow_value("lamiygmail.@com").for(:email) }
     it { should validate_presence_of(:role) }
-    it { should define_enum_for(:role).with_values(%i[admin teacher student])}
+    it { should define_enum_for(:role).with_values(%i[admin teacher student]) 
   end
 
   describe "associations" do
     it { should have_many(:classrooms).with_foreign_key(:user_id).dependent(:destroy) }
     it { should have_many(:enrollments).dependent(:destroy) }
-    it { should have_many(:classrooms_as_student).through(:enrollments).source(:classroom) } # for students
+    it { should have_many(:classrooms_as_student).through(:enrollments).source(:classroom) }
     it { should have_many(:posts).dependent(:destroy) }
     it { should have_many(:comments).dependent(:destroy) }
   end
-
-
-
-
-
 end
