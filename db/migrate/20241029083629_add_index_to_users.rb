23b18975f477a55fd9af342b6139e7f1612f49cb
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-29 14:59:45 +0600

    made some changes to the AddIndexToUser migration file

--- db/migrate/20241029083629_add_index_to_users.rb
+++ db/migrate/20241029083629_add_index_to_users.rb
@@ -1,11 +1,11 @@
 class AddIndexToUsers < ActiveRecord::Migration[7.2]
   def up
     add_index :users, :email, unique: true
-    add_index :users, :role unless index_exists?(:users, :role)
+    add_index :users, :role
   end
 
   def down
     remove_index :users, :email
-    remove_index :users, :role if index_exists?(:users, :role)
+    remove_index :users, :role
   end
 end
