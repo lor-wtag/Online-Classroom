a173c93ef4c2ffa248fc7343bc8489f4680d6771
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-29 14:41:41 +0600

    Modified the migrations to using symbol notation

--- db/migrate/20241023082904_create_assignments.rb
+++ db/migrate/20241023082904_create_assignments.rb
@@ -1,11 +1,11 @@
 class CreateAssignments < ActiveRecord::Migration[7.2]
   def up
     create_table :assignments do |t|
-      t.string "title"
-      t.text "description"
-      t.date "due_date"
-      t.integer "classroom_id"
-      t.integer "user_id"
+      t.string :title
+      t.text :description
+      t.date :due_date
+      t.integer :classroom_id
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_23_084854) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_083629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -105,6 +105,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_23_084854) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["role"], name: "index_users_on_role"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
