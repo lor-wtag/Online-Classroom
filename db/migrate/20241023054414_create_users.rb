842bc0a22b918c7248269a5ce2cb1467b87acaef
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-29 14:41:41 +0600

    Modified the migrations to using symbol notation

--- db/migrate/20241023054414_create_users.rb
+++ db/migrate/20241023054414_create_users.rb
@@ -1,13 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def up
     create_table :users do |t|
-      t.string "name"
-      t.string "email"
-      t.integer "role", default: 2
-
-      t.string "password_digest"
+      t.string :name
+      t.string :email
+      t.integer :role, default: 2
+      t.string :password_digest
       t.timestamps
     end
+    add_index :email
   end
 
   def down
--- db/migrate/20241023081215_create_classrooms.rb
+++ db/migrate/20241023081215_create_classrooms.rb
@@ -1,10 +1,10 @@
 class CreateClassrooms < ActiveRecord::Migration[7.2]
   def up
     create_table :classrooms do |t|
-      t.integer "user_id"
-      t.string "name"
-      t.string "course_code"
-      t.string "classroom_code"
+      t.integer :user_id
+      t.string :name
+      t.string :course_code
+      t.string :classroom_code
       t.timestamps
     end
   end
