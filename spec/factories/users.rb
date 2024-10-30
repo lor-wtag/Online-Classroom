a75f53df48e6421a2ade18d8f942e5d9641f2075
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-30 14:10:52 +0600

    added a factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name {"Jane Doe"}
-    email {"janedoe@gmail.com"}
+    name{"Jane Doe"}
+    # sequence(:email) { |n| "user_no#{n}@gmail.com" }
+    email{"janedoes@gmail.com"}
     role{:student}
     password {"123456789"}
   end
