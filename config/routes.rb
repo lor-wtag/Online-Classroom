9a8621239edae2cad350466e71f32ea1ca5f76a5
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-30 14:11:39 +0600

    added landing page to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "sessions/login"
   get "sessions/logout"
   post "sessions/attempt_login"
-  get "sessions/landing_page"
+  get "home/landing_page"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
