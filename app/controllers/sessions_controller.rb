5ecea8ee84097ec27e7f85f8bf529c11f782405b
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-31 17:35:54 +0600

    Updated code for login, logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,17 @@ class SessionsController < ApplicationController
     logout current_user
     redirect_to root_path, notice: "You have been logged out!"
   end
-  
+
   def new
+  end
+
+  def create
+      if user=User.authenticate_by(email: params[:email], password: params[:password])
+        login user
+        redirect_to root_path, notice: "You have logged in! :D"
+      else
+        flash[:alert]="Invalid username/password"
+        render :new, status: :unprocessable_entity
+      end
   end
 end
