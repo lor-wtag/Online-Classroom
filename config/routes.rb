116868b0f8a1162e9bae7f286c3a80dce1c674ee
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-10-31 17:36:08 +0600

    added necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
   root "home#landing_page"
   resources :users
   resources :sessions
+  resources :passwords
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
