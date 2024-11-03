d851475281bc8b3e255364fbb7e7abf6e0d58e07
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-03 11:41:56 +0600

    added code for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+
+  before_action :user_not_logged_in?
+  before_action :set_user_by_token, only: [:edit, :update]
   def new
   end
 
@@ -8,20 +11,33 @@ class PasswordResetsController < ApplicationController
         user: user,
         token: user.generate_token_for(:password_reset)
       ).password_reset.deliver_later
+      redirect_to root_path, notice: "Check your email to reset your password"
+    else
+      render :new, status: :unprocessable_entity
     end
+    logger.debug "-----------------------------------------------------------------------------------------------------------ID: #{params[:id]}, Token: #{params[:token]}"
 
-    redirect_to root_path, notice: "Check your email to rest your password"
+    
   end
 
   def edit
   end
 
   def update
+    if @user.update(password_params)
+      redirect_to new_session_path, notice: " Your Password has been reset! Please login"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
   def set_user_by_token
     @user=User.find_by_token_for(:password_reset, params[:token])
-    redirect_to new_password_resets_path unless @user.present?
+    redirect_to new_password_reset_path alert:" Invalid token. Please try again" unless @user.present?
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
