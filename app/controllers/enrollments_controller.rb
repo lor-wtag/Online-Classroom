10617cd2da82eda38ca92c8ba9c191cb78899bca
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-06 10:02:51 +0600

    minor changes for rubocop

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,28 +1,19 @@
 class EnrollmentsController < ApplicationController
   def delete
-    @classroom = Classroom.find(params[:classroom_id]) # Get the classroom using classroom_id
-    @user = User.find(params[:user_id])                # Get the user using user_id
-    @enrollment = Enrollment.find_by(classroom_id: @classroom.id, user_id: @user.id) # Find the correct enrollment
-
-    puts "Enrollment: #{@enrollment.inspect}"
-    puts "Classroom: #{@classroom.inspect}"
-    puts "----------------------------------------------##Params: #{params.inspect}"
+    @classroom = Classroom.find(params[:classroom_id])
+    @user = User.find(params[:user_id])
+    @enrollment = Enrollment.find_by(classroom_id: @classroom.id, user_id: @user.id)
   end
-  
+
   def destroy
-    puts "ami ki destroy te ashchi######################################################"
-    puts "----------------------------------------------Params: #{params.inspect}"
+    @classroom = Classroom.find(params[:classroom_id])
+    @enrollment = Enrollment.find_by(classroom_id: @classroom.id, user_id: params[:user_id])
 
-    @classroom = Classroom.find(params[:classroom_id]) # Get the classroom using classroom_id
-    @enrollment = Enrollment.find_by(classroom_id: @classroom.id, user_id: params[:user_id]) # Correctly find enrollment
-
-    puts "------------------------------------------------------Helloooo#{@enrollment.inspect}"
-    
     if @enrollment && !current_user.student?
       @enrollment.destroy
       redirect_to classroom_path(@classroom), notice: "Student removed from this classroom"
     else
-      redirect_to classrooms_path, alert: "Enrollment not found!" # Use redirect instead of render for consistency
+      redirect_to classrooms_path, alert: "Enrollment not found!"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
       post :send_invitations
       get :join
     end
-
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
