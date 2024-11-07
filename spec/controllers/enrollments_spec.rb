5d6aa1a977ca896599a35a3ec1c424f19e97b378
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-07 17:10:37 +0600

    removed unnecessary comments

--- spec/controllers/enrollments_spec.rb
+++ spec/controllers/enrollments_spec.rb
@@ -12,16 +12,9 @@ RSpec.describe EnrollmentsController, type: :controller do
 
   describe "DELETE #destroy" do
     it "removes the student from the classroom" do
-      # enrollment_to_delete = Enrollment.find_by(user_id: student.id, classroom_id: classroom.id)
-      # puts "++++++++++++++++#{enrollment_to_delete.user.name}"
-      # expect(enrollment_to_delete).not_to be_nil
       expect {
         delete :destroy, params: { classroom_id: classroom.id, id: enrollment.id }
     }.to change(Enrollment, :count).by(-1)
-      # expect(Enrollment.find_by(id: enrollment_to_delete.id)).to be_nil
-
-      # expect(response).to redirect_to(classroom_path(classroom))
-      # expect(flash[:notice]).to eq("Student removed from this classroom")
     end
   end
 end
