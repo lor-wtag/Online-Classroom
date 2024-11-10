1d44ee48bf4f2b137f2d4a81da653e6790477f93
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-10 14:24:36 +0600

    added leaving the classroom

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,4 +1,5 @@
 class EnrollmentsController < ApplicationController
+  before_action :authenticate_user!
   def delete
     @classroom = Classroom.find(params[:classroom_id])
     @enrollment = Enrollment.find_by(classroom_id: @classroom.id,  id: params[:id])
@@ -6,11 +7,17 @@ class EnrollmentsController < ApplicationController
 
   def destroy
     @classroom = Classroom.find(params[:classroom_id])
-    @enrollment = Enrollment.find_by(id: params[:id], classroom_id: @classroom.id)
-
-    if @enrollment && !current_user.student?
+    @enrollment = Enrollment.find_by(classroom_id: @classroom.id,  id: params[:id])
+    if @enrollment
+      if !current_user.student?
       @enrollment.destroy
       redirect_to classroom_path(@classroom), notice: "Student removed from this classroom"
+      elsif current_user.student?
+        @enrollment.destroy
+        redirect_to classrooms_path, notice: "You have left the classroom #{@classroom.name}"
+      else
+        flash[:alert]="Action failed! try again"
+      end
     else
       redirect_to classrooms_path, alert: "Enrollment not found!"
     end
