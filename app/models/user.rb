bafc4bbf6bcc49cd9e9533589f6d007c41691c2d
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-10 10:41:26 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,24 @@
 require "digest"
 
 class User < ApplicationRecord
-  attr_accessor :password
-  has_one_attached :profile_picture
   before_save :hashPassword
+
+  attr_accessor :password
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i
   enum :role, { admin: 0, teacher: 1, student: 2 }
-  validates :name, presence: true
-  validates :email, presence: true, length: { in: 10..50 }, format: { with: EMAIL_REGEX }, uniqueness: true
-  validates :role, presence: true, inclusion: { in: roles.keys }
-  validates :password, presence: true, on: :create, length: { in: (8..20) }
+
+  has_one_attached :profile_picture
   has_many :classrooms, foreign_key: :user_id, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :enrollments, dependent: :destroy
   has_many :classrooms_as_student, through: :enrollments, source: :classroom
 
+  validates :name, presence: true
+  validates :email, presence: true, length: { in: 10..50 }, format: { with: EMAIL_REGEX }, uniqueness: true
+  validates :role, presence: true, inclusion: { in: roles.keys }
+  validates :password, presence: true, on: :create, length: { in: (8..20) }
+  
   def hashPassword
     if password.present?
       self.password_digest = Digest::SHA256.hexdigest(password)
