adb28dc7f3ae98a1a539fb3cb7df9d28c21bba5c
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-10 10:20:06 +0600

    organized the model files

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,8 +1,10 @@
 class Assignment < ApplicationRecord
+  belongs_to :classroom
+
+  has_many :submissions, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+  
   validates :title, presence: true, length: { in: (5..200) }
   validates :classroom_id, presence: true
 
-  belongs_to :classroom
-  has_many :submissions, dependent: :destroy
-  has_many :comments, as: :commentable, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :classroom
+  belongs_to :user
+
+  has_many :comments, as: :commentable, dependent: :destroy
+  
   validates :title, presence: true, length: { in: (5..100) }
   validates :content, presence: true, length: { in: (5..300) }
   validates :user_id, presence: true
   validates :classroom_id, presence: true
 
-  belongs_to :classroom
-  belongs_to :user
-  has_many :comments, as: :commentable, dependent: :destroy
 end
