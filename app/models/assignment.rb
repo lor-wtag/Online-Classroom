7409adba45d994d6a997884ee2527f50ca5c336f
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2024-11-18 17:08:04 +0600

    minor last minute changes

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,8 +7,15 @@ class Assignment < ApplicationRecord
 
   validates :title, presence: true, length: { in: (5..200) }
   validates :classroom_id, presence: true
+  validate :due_date_cannot_be_in_the_past
 
   def self.ransackable_attributes(auth_object = nil)
     %w[index title description due_date]
+  end
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Time.now
+      errors.add(:due_date, "can't be in the past")
+    end
   end
 end
