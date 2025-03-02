51dc30336a22cd7eea16480f205da5ea6a11d3e6
Lamiya Tahsin Orpa <lamiya.orpa@welldev.io>
2025-03-02 11:05:38 +0600

    demo changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def submission_status(submission)
+    if submission.late
+      content_tag(:span, "Late", class: "late-submission")
+    else
+      content_tag(:span, "On Time", class: "on-time-submission")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_10_111208) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_02_045943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_10_111208) do
     t.datetime "graded_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "late"
   end
 
   create_table "users", force: :cascade do |t|
